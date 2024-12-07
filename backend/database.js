77520cc6b7bc48e8869a9cd2ffa7fdaccd2fa719
Rajuprasad748 <149618288+Rajuprasad748@users.noreply.github.com>
2024-12-07 15:54:57 +0530

    Update database.js
    remove all logs

--- backend/database.js
+++ backend/database.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 const mongodbConnection = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/mernapp");
-    console.log("Connected to MongoDB");
+   
 
     const collection = mongoose.connection.db.collection("foodItems");
     const data = await collection.find({}).toArray();
 
-    console.log("data:");
 
     const foodCategory = mongoose.connection.db.collection("foodcategory");
     const catData = await foodCategory.find({}).toArray();
