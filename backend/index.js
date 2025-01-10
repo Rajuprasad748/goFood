71e7153ba77610fcc2a11972327e0be03a785591
Rajuprasad748 <149618288+Rajuprasad748@users.noreply.github.com>
2025-01-10 13:13:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.use((req,res,next)  => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://peaceful-valkyrie-748f91.netlify.app/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept');
   next();
 })
