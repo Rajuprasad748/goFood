44d7220433f3e8820fdc495c0e99acab3b22cb2b
Rajuprasad748 <149618288+Rajuprasad748@users.noreply.github.com>
2025-01-10 12:27:29 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -19,7 +19,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://gofood-3-8rvz.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
