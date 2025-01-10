e87ce077eb52aaf4f382b85ecad3cab8549aa327
Raju Prasad <rp0071085@gmail.com>
2025-01-10 13:18:40 +0530

    kgj

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000;
+const cors = require('cors');
 const mongodb = require("./database")
 
 mongodb();
@@ -8,11 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use((req,res,next)  => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept');
-  next();
-})
+app.use(cors());
 
 
 
